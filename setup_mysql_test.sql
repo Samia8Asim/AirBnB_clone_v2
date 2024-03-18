be9ca3817764136ccca5bc05a1c72a62e56bffb9
rawan-omer <rawanomer0119@gmail.com>
2024-03-18 16:19:48 -0700

    alx-programming

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- Create database hbnb_test_db if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create user hbnb_test if it doesn't exist and set the password
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on the database hbnb_test_db to user hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to user hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
