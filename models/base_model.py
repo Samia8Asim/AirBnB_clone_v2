691e66dc310a9c9b4648ab656dd43fa9c28487b7
rawan-omer <rawanomer0119@gmail.com>
2024-03-19 07:06:06 -0700

    still codeing

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module defines base class for all models in our hbnb clone"""
 import uuid
-import os
 from datetime import datetime
 import models
 from sqlalchemy import Column, String, DateTime, Integer
@@ -13,58 +12,40 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False, unique=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            models.storage.new(self)
-        else:
+        """Initializes BaseModel instance"""
+        if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
+                setattr(self, key, value)
             if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
-            else:
-                self.id = kwargs['id']
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                kwargs['created_at'] = datetime.now()
-
-            if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                kwargs['updated_at'] = datetime.now()
-
-            self.__dict__.update(kwargs)
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls_name = self.__class__.__name__
-        return '[{}] ({}) {}'.format(cls_name, self.id, self.__dict__)
+            if 'created_at' not in kwargs:
+                self.created_at = self.updated_at = datetime.utcnow()
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """Saves the instance to the storage"""
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
+    def delete(self):
+        """Deletes the instance from the storage"""
+        models.storage.delete(self)
+
     def to_dict(self):
-        """Convert instance into dict format"""
+        """Returns a dictionary representation of the instance"""
         dictionary = dict(self.__dict__)
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
         dictionary.pop('_sa_instance_state', None)
         return dictionary
 
-    def delete(self):
-        """Deletes BaseModel instance from the storage"""
-        models.storage.delete(self)
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+
+    def __repr__(self):
+        """Returns a string representation of the instance"""
+        return str(self)
--- models/state.py
+++ models/state.py
@@ -2,30 +2,27 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 import os
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
-            'City',
-            cascade='all, delete, delete-orphan',
-            backref='state'
-        )
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete-orphan', backref='state')
     else:
+        __tablename__ = 'states'
+        name = ""
+
         @property
         def cities(self):
-            """Returns the cities in this State"""
+            """ Getter attribute for cities in State """
             from models import storage
-            cities_in_state = []
-            for value in storage.all(City).values():
-                if value.state_id == self.id:
-                    cities_in_state.append(value)
-            return cities_in_state
+            cities_list = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
