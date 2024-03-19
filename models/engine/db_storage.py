3df6d950cc31d82c95089ede01708dcba5367829
rawan-omer <rawanomer0119@gmail.com>
2024-03-19 07:31:20 -0700

    still codeing

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,63 +19,56 @@ class DBStorage:
 
     def __init__(self):
         """Initialize DBStorage"""
-        db_user = os.getenv('HBNB_MYSQL_USER')
-        db_password = os.getenv('HBNB_MYSQL_PWD')
-        db_host = os.getenv('HBNB_MYSQL_HOST', default='localhost')
-        db_name = os.getenv('HBNB_MYSQL_DB')
-        db_env = os.getenv('HBNB_ENV')
-
         self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}'.
-            format(db_user, db_password, db_host, db_name),
-            pool_pre_ping=True)
-
-        if db_env == 'test':
+            'mysql+mysqldb://{}:{}@{}/{}'
+            .format(os.getenv('HBNB_MYSQL_USER'),
+                    os.getenv('HBNB_MYSQL_PWD'),
+                    os.getenv('HBNB_MYSQL_HOST'),
+                    os.getenv('HBNB_MYSQL_DB')),
+            pool_pre_ping=True
+        )
+        if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(bind=self.__engine)
 
-        session_factory = sessionmaker(
-            bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(session_factory)
-
     def all(self, cls=None):
-        """Query on current database"""
-        from models import classes_dict
-        objects_dict = {}
+        """ Query all objects """
+        from models import classes
+        objects = {}
         if cls:
-            if type(cls) == str:
-                cls = classes_dict.get(cls)
-            query_result = self.__session.query(cls).all()
+            query = self.__session.query(classes[cls])
+            for obj in query.all():
+                key = "{}.{}".format(type(obj).__name__, obj.id)
+                objects[key] = obj
         else:
-            all_classes = [State, City, User, Place, Amenity, Review]
-            query_result = []
-            for class_obj in all_classes:
-                query_result.extend(self.__session.query(class_obj).all())
-
-        for obj in query_result:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            objects_dict[key] = obj
-
-        return objects_dict
+            for cls in classes.values():
+                query = self.__session.query(cls)
+                for obj in query.all():
+                    key = "{}.{}".format(type(obj).__name__, obj.id)
+                    objects[key] = obj
+        return objects
 
     def new(self, obj):
-        """Add object to the current database session"""
-        self.__session.add(obj)
+        """ Add the object to the current database session """
+        if obj:
+            self.__session.add(obj)
 
     def save(self):
-        """Commit changes of current database session"""
+        """ Commit all changes of the current database session """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete from the current database"""
+        """ Delete from the current database session """
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables in the database and current database session"""
+        """ Create all tables in the database """
         Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(sessionmaker(
-            bind=self.__engine, expire_on_commit=False))
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
 
     def close(self):
-        """close session"""
-        self.__session.close()
+        """ Close the session """
+        self.__session.remove()
