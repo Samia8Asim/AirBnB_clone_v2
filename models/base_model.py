d595e6a1dc1f03813ff518ac3b8870b59385dd27
rawan-omer <rawanomer0119@gmail.com>
2024-03-19 07:16:55 -0700

    still codeing

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines base class for all models in our hbnb clone"""
 import uuid
+import os
 from datetime import datetime
 import models
 from sqlalchemy import Column, String, DateTime, Integer
