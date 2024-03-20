1fedf79bcff73ba408aea122217b8e10bc7f1dc2
rawan-omer <rawanomer0119@gmail.com>
2024-03-20 05:00:05 -0700

    fixing the codes

--- models/state.py
+++ models/state.py
@@ -3,28 +3,30 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from models import storage
+from models.city import City
+import os
 
 
 class State(BaseModel, Base):
     """State class representing states"""
     __tablename__ = 'states'
 
-    name = Column(String(128), nullable=False)
-
-    # For DBStorage
-    if storage_type == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-
-    # For FileStorage
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City',
+            cascade='all, delete, delete-orphan',
+            backref='state'
+        )
     else:
         @property
         def cities(self):
-            """Getter attribute to retrieve cities related to the state"""
-            cities_list = []
-            all_cities = storage.all('City')
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
+            """Returns the cities in this State"""
+            from models import storage
+            cities_in_state = []
+            for value in storage.all(City).values():
+                if value.state_id == self.id:
+                    cities_in_state.append(value)
+            return cities_in_state
