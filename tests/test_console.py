4c3f85138cc7b53b3fe8f71f55f79d776f0cc274
Samia8Asim <samia2003.8@gmail.com>
2024-03-20 22:45:43 +0200

    3

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,41 +10,44 @@ from models.user import User
 from io import StringIO
 
 
-class TestHBNBCommand(unittest.TestCase):
-    @unittest.skipIf(
-            os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
-    def test_db_create_city(self):
-        """Test creating a City instance with DBStorage."""
-        with patch('sys.stdout', new=StringIO()) as cout:
-            HBNBCommand().onecmd('create City name="Paris" state_id="123"')
-            output = cout.getvalue().strip()
-            self.assertTrue(len(output) == 36)
+class TestConsole(unittest.TestCase):
+    """test console mosule"""
+    @classmethod
+    def setUpClass(cls):
+        """Setup the test"""
+        cls.console_instance = HBNBCommand()
 
-            city_id = output
-            city = storage.get(City, city_id)
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down after the test"""
+        del cls.console_instance
 
-            self.assertIsInstance(city, City)
-            self.assertEqual(city.name, "Paris")
-            self.assertEqual(city.state_id, "123")
+    def tearDown(self):
+        """Remove the temp (file.json) created during the test"""
+        if (os.getenv('HBNB_TYPE_STORAGE') != 'db'):
+            try:
+                os.remove("file.json")
+            except Exception:
+                pass
 
-    @unittest.skipIf(
-            os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
-    def test_db_create_user(self):
-        """Test creating a User instance with DBStorage."""
-        with patch('sys.stdout', new=StringIO()) as cout:
-            HBNBCommand().onecmd(
-                    'create User email="test@example.com"\
-                    password="password123"'
-            )
-            output = cout.getvalue().strip()
-            self.assertTrue(len(output) == 36)
+    def test_console_module_has_docstrings(self):
+        """Test if the console module and its methods"""
+        self.assertIsNotNone(console.__doc__)
+        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_show.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_count.__doc__)
 
-            user_id = output
-            user = storage.get(User, user_id)
-
-            self.assertIsInstance(user, User)
-            self.assertEqual(user.email, "test@example.com")
-            self.assertEqual(user.password, "password123")
+    def test_emptyline_command(self):
+        """test behavior of empty line"""
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            self.console_instance.onecmd("\n")
+            self.assertEqual('', mock_stdout.getvalue())
 
 
 if __name__ == '__main__':
